e8088179b1816323d42964e0259ce60d658b1689
Ashley <ashleykeeling2020@gmail.com>
2024-07-13 18:27:56 +0100

    added protection to react routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './Hooks/useAuthContext';
+
 import NavBar from './Components/NavBar';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
@@ -6,6 +8,8 @@ import Signup from './Pages/Signup';
 import UpdateFoodItem from './Pages/updateFoodItem';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -14,19 +18,19 @@ function App() {
           <Routes>
             <Route
               path='/'
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to='/login' />}
             />
             <Route
               path='/login'
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to='/' />}
             />
             <Route
               path='/signup'
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to='/' />}
             />
             <Route
               path='/updateFoodItem'
-              element={<UpdateFoodItem />}
+              element={user ? <UpdateFoodItem /> : <Navigate to='/login' />}
             />
           </Routes>
         </div>
