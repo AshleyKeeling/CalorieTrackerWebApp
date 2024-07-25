d287d24eaf829c0e4067023f4e906e286cfd9552
Ashley <ashleykeeling2020@gmail.com>
2024-07-25 20:36:37 +0100

    added middleware function as a user auth route so the jwt token can be verified

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const requireAuth = require('../middleware/requireAuth');
 // controller
 const { signupUser, loginUser } = require('../controllers/userController');
 
@@ -11,5 +11,9 @@ router.post('/login', loginUser)
 // sign up route
 router.post('/signup', signupUser)
 
+// verify token for client
+router.post('/validate-token', requireAuth, (req, res) => {
+    res.status(200).json({ isValid: true });
+});
 
 module.exports = router;
